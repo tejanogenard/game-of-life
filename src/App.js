60ec6fce5d605f2ef86193769c40470878afc5c5
Genard Ivan Tejano <genardivanteajno@Genards-MacBook-Pro.local>
2020-07-28 11:25:41 -0700

    can toggle cells, added start & stop button

--- src/App.js
+++ src/App.js
@@ -1,38 +1,63 @@
-import React, {useState} from 'react';
-
+import React, { useState, useCallback } from 'react';
+import produce from 'immer'
 
 const numRows = 50;
-const numCols = 50; 
+const numCols = 50;
 
 
 function App() {
   const [grid, setGrid] = useState(() => {
     const rows = []
-    for (let i = 0; i < numRows; i ++ ){
+    for (let i = 0; i < numRows; i++) {
       rows.push(Array.from(Array(numCols), () => 0))
     }
     return rows
   })
 
+  const [running, setRunning] = useState(false)
+
+  const runSimulation = useCallback(() => {
+
+    if (!running) {
+      return
+    }
+    // simulate the game of life
+    setTimeout(runSimulation, 1000)
+  }, [])
+
   return (
-    <div style = {{
-      display: 'grid',
-      gridTemplateColumns: `repeat(${numCols}, 20px`
-    }}>
-      {grid.map((rows, i) =>
-       rows.map((col, k) => (
-        <div 
-          key ={`${i} - ${k}`}
-          style = {{
-             width: 20, 
-             height: 20,
-             backgroundColor: grid[i][k] ? "pink": undefined,
-             border: 'solid 1px black'
-             }}
+    <>
+      <button onClick={() => {
+        setRunning(!running)
+      }}
+      >
+        {running ? 'stop' : 'start'}
+      </button>
+      <div style={{
+        display: 'grid',
+        gridTemplateColumns: `repeat(${numCols}, 20px`
+      }}>
+        {grid.map((rows, i) =>
+          rows.map((col, k) => (
+            <div
+              key={`${i} - ${k}`}
+              onClick={() => {
+                const newGrid = produce(grid, gridCopy => {
+                  gridCopy[i][k] = grid[i][k] ? 0 : 1
+                })
+                setGrid(newGrid)
+              }}
+              style={{
+                width: 20,
+                height: 20,
+                backgroundColor: grid[i][k] ? "pink" : undefined,
+                border: 'solid 1px black'
+              }}
             />
           ))
         )}
-    </div>
+      </div>
+    </>
   );
 }
 
